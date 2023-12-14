d797a8d7f9b18acc9927e7b52a090b5f36bb62b5
Alexey Logachev <apelsin.alexey@gmail.com>
2023-12-14 15:53:13 +0400

    Final corrections and comments

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿
-int[,] RandomMatrix(int rows, int cols)
+int[,] RandomMatrix(int rows, int cols) //Функция создания матрицы из рандомных чисел от 1 до 9, где указываем количество строк и столбцов
 {
     Random random = new Random();
     int[,] array = new int[rows, cols];
@@ -11,7 +11,7 @@ int[,] RandomMatrix(int rows, int cols)
     return array;
 }
 
-void PrintMatrix(int[,] matrix)
+void PrintMatrix(int[,] matrix) //Функция вывода матрицы в терминал
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
@@ -23,57 +23,14 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
-void SquareEvenIndexedElements(int[,] matrix)
+void PrintElementOnPosition(int[,] matrix, int row, int col) //Функция, которая выводит элемент по введенным позициям. Применяется в первой задаче
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            if ((i % 2 == 0) && (j % 2 == 0))
-                matrix[i, j] *= matrix[i, j];
-        }
-    }
-}
-
-int SumOfDiagonalElements(int[,] matrix)
-{
-    int sum = 0;
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            if (i == j)
-                sum += matrix[i, j];
-        }
-    }
-    return sum;
-}
-
-int[] AverageElementsInRowArray(int[,] matrix)
-{
-    int[] averages = new int[matrix.GetLength(0)];
-
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        int sum = 0;
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            sum += matrix[i, j];
-        }
-        averages[i] = sum / matrix.GetLength(1);
-    }
-
-    return averages;
-}
-
-void PrintElementOnPosition(int[,] matrix, int row, int col)
-{
-    if (row < 0 || col < 0)
+    if (row < 0 || col < 0) //проверка, что пользователь не ввел отрицательные числа
     {
         Console.WriteLine("Rows and Columns should be >= 0");
         return;
     }
-    if (row >= matrix.GetLength(0) || col >= matrix.GetLength(1))
+    if (row >= matrix.GetLength(0) || col >= matrix.GetLength(1)) //проверка, что не выходим за границы матрицы
     {
         Console.WriteLine("There is no such position in the matrix!");
         return;
@@ -82,7 +39,7 @@ void PrintElementOnPosition(int[,] matrix, int row, int col)
     Console.WriteLine($"Element on position [{row}, {col}] is {matrix[row, col]}.");
 }
 
-void ChangeFirstRowToLastInMatrix(int[,] matrix)
+void ChangeFirstRowToLastInMatrix(int[,] matrix) //Функция, которая меняет первый и последний ряды матрицы. Применяется во второй задаче
 {
     for (int i = 0; i < matrix.GetLength(1); i++)
     {
@@ -92,7 +49,7 @@ void ChangeFirstRowToLastInMatrix(int[,] matrix)
     }
 }
 
-int MinSumElementsInRow(int[,] matrix)
+int MinSumElementsInRow(int[,] matrix) //Функция, которая находит строку с минимальной суммой элементов. Применяется в третьей задаче
 {
     int minSum = int.MaxValue;
     int minRowIndex = -1;
@@ -113,69 +70,7 @@ int MinSumElementsInRow(int[,] matrix)
     return minRowIndex;
 }
 
-// Задайте двумерный массив. Найдите элементы, у которых оба
-// индекса чётные, и замените эти элементы на их квадраты.
-
-int[,] userMatrix = RandomMatrix(3, 3);
-PrintMatrix(userMatrix);
-SquareEvenIndexedElements(userMatrix);
-Console.WriteLine();
-PrintMatrix(userMatrix);
-
-// Задайте двумерный массив. Найдите сумму элементов,
-// находящихся на главной диагонали (с индексами (0,0); (1;1) и
-// т.д.
-
-int total = SumOfDiagonalElements(userMatrix);
-Console.WriteLine();
-Console.WriteLine(total);
-
-
-// Задайте двумерный массив из целых чисел. Сформируйте новый
-// одномерный массив, состоящий из средних арифметических
-// значений по строкам двумерного массива. 
-
-int[] userArray = AverageElementsInRowArray(userMatrix);
-for (int i = 0; i < userArray.Length; i++)
-{
-    Console.Write($"{userArray[i]} ");
-}
-
-// Задача 1: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
-// и возвращает значение этого элемента или же указание, что такого элемента нет.
-
-int[,] newMatrix = RandomMatrix(4, 5);
-
-Console.WriteLine();
-PrintMatrix(newMatrix);
-Console.WriteLine();
-PrintElementOnPosition(newMatrix, 3, 3);
-PrintElementOnPosition(newMatrix, -1, 2);
-PrintElementOnPosition(newMatrix, 5, 5);
-
-// Задача 2: Задайте двумерный массив. 
-// Напишите программу, которая поменяет местами первую и последнюю строку массива.
-
-ChangeFirstRowToLastInMatrix(newMatrix);
-Console.WriteLine();
-PrintMatrix(newMatrix);
-
-// Задача 3: Задайте прямоугольный двумерный массив. 
-// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
-
-int[,] taskMatrix = RandomMatrix(4, 4);
-PrintMatrix(taskMatrix);
-int minRow = MinSumElementsInRow(taskMatrix);
-Console.WriteLine($"Row with the minimum sum is: {minRow}");
-
-// Задача 4*(не обязательная): Задайте двумерный массив из целых чисел. 
-// Напишите программу, которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива. 
-// Под удалением понимается создание нового двумерного массива без строки и столбца
-
-int[,] forthMatrix = RandomMatrix(6, 6);
-PrintMatrix(forthMatrix);
-
-int[,] MatrixWithRemovedRowsAndCols(int[,] matrix, int[] position)
+int[,] MatrixWithRemovedRowsAndCols(int[,] matrix, int[] position) //Функция, которая удаляет строку и столбец по элементу, который вводим.
 {
     int rows = matrix.GetLength(0);
     int cols = matrix.GetLength(1);
@@ -199,7 +94,7 @@ int[,] MatrixWithRemovedRowsAndCols(int[,] matrix, int[] position)
     return newMatrix;
 }
 
-int[] FindPositionOfMinElement(int[,] matrix)
+int[] FindPositionOfMinElement(int[,] matrix) // Функция, которая определяет минимальный элемент в матрице, и возвращает массив с положением этого элемента
 {
     int minElement = int.MaxValue;
     int[] array = new int[2];
@@ -218,8 +113,50 @@ int[] FindPositionOfMinElement(int[,] matrix)
     return array;
 }
 
+
+// Задача 1: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Console.WriteLine("First task. Find Element on position.");
+int[,] firstMatrix = RandomMatrix(4, 5);
+PrintMatrix(firstMatrix);
+Console.WriteLine();
+PrintElementOnPosition(firstMatrix, 3, 3);
+PrintElementOnPosition(firstMatrix, -1, 2);
+PrintElementOnPosition(firstMatrix, 5, 5);
+
+// Задача 2: Задайте двумерный массив. 
+// Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+int[,] secondMatrix = RandomMatrix(5,5);
+Console.WriteLine();
+Console.WriteLine("Second task. Swap first row with last row.");
+PrintMatrix(secondMatrix);
+ChangeFirstRowToLastInMatrix(secondMatrix);
+Console.WriteLine();
+PrintMatrix(secondMatrix);
+
+// Задача 3: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int[,] thirdMatrix = RandomMatrix(4, 4);
+Console.WriteLine();
+Console.WriteLine("Third task. Find row with minimum sum of elements in it");
+PrintMatrix(thirdMatrix);
+int minRow = MinSumElementsInRow(thirdMatrix);
+Console.WriteLine();
+Console.WriteLine($"Row with the minimum sum is: {minRow}");
+
+// Задача 4*(не обязательная): Задайте двумерный массив из целых чисел. 
+// Напишите программу, которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива. 
+// Под удалением понимается создание нового двумерного массива без строки и столбца
+
+Console.WriteLine();
+Console.WriteLine("Forth task. Delete row and column there min element is");
+int[,] forthMatrix = RandomMatrix(6, 6);
+PrintMatrix(forthMatrix);
 int[] position = FindPositionOfMinElement(forthMatrix);
-Console.WriteLine($"{position[0]}, {position[1]}");
+Console.WriteLine($"Min Element is on position: [{position[0]}, {position[1]}]");
 int[,] fixedMatrix = MatrixWithRemovedRowsAndCols(forthMatrix, position);
 Console.WriteLine();
 PrintMatrix(fixedMatrix);
